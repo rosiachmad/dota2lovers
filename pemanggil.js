f873a8a53fd420f9a9cd46bdb0138519ab1d07c3
rosiachmad <ealbri4@my.wgu.edu>
2018-03-22 15:44:03 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/03/chili.html"}
 if(x=="2"){xxx="/2018/03/onion.html"}
 if(x=="3"){xxx="/2018/03/starfruit.html"}
 if(x=="4"){xxx="/2018/03/cinnamon.html"}
-if(x=="5"){xxx="/2018/03/ginger.html"}a_to_ve[i].href="https://nyongbae-safelink.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/03/ginger.html"}a_to_ve[i].href="https://nyongbae-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
